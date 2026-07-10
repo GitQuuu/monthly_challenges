9544bedc654ec169a01d97242d38f0928d7c43c9
Qu <Quanvle@live.dk>
2026-07-10 02:40:48 +0200

    23. Adding more views & URLS

--- challenges/views.py
+++ challenges/views.py
@@ -2,5 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def january(request):
     return HttpResponse("Hello, world!")
+
+def february(request):
+    return HttpResponse("February")
